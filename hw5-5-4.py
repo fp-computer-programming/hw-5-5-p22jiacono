bc0b69492006c0d85d8e1a5a63b9fbf7029298c1
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-11-03 16:25:39 -0400

    add file 5-5-4

---
+++ hw5-5-4.py
@@ -0,0 +1,11 @@
+#Author:JTI 11/3/21
+
+word1 = input("Enter a word: ")
+word2 = input("Enter a differnt word: ")
+word3 = input("Enter another differnt word:")
+
+string1 = word1.lowercase()
+string2 = word2.lowercase()
+string3 = word3.lowercase()
+
+if string1 < 
