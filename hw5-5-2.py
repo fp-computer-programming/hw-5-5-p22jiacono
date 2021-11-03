b02d4b99b7296993837ce65597b7210795354979
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-11-03 16:24:34 -0400

    add file hw5-5-2

---
+++ hw5-5-2.py
@@ -0,0 +1,10 @@
+#Author:JTI 11/3/21
+
+word = input("ENter a 6 six letter word: ")
+
+odd = word[1] + "_" + word[3] + "_" + word[5]
+
+even = word[2] + "_" + word[4] + "_" + word[6]
+
+print(even)
+print(odd)
